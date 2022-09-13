65f2848b0680d41791e5593968b955d304d2d7bb
alireza <alirezaahoun@gmail.com>
2022-09-13 07:43:23 -0700

    fix files

--- resources/views/home/layouts/master.blade.php
+++ resources/views/home/layouts/master.blade.php
@@ -24,12 +24,12 @@
       crossorigin="anonymous"
     />
 
-    <link
+    {{-- <link
       rel="stylesheet"
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossorigin="anonymous"
-    />
+    /> --}}
     <link
       rel="stylesheet"
       href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
@@ -79,5 +79,6 @@
 
     <!-- Javascript -->
     <script src="{{ asset('js/home.js') }}"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/wow/0.1.12/wow.min.js"></script>
   </body>
 </html>
