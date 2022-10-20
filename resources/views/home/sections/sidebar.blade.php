7673d96b4e102d979049a2aeb55eaf7f6a55c6c2
sohrab <sohrabnasehi.sn@gmail.com>
2022-10-21 01:38:15 +0330

    added vijegi mahsool col

--- resources/views/home/sections/sidebar.blade.php
+++ resources/views/home/sections/sidebar.blade.php
@@ -6,7 +6,7 @@
 
     <div class="logo">
       <h3>
-        <a href="index.html"
+        <a href="{{ route('home.pages.index') }}"
           >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم</a
         >
       </h3>
@@ -14,37 +14,37 @@
 
     <ul class="list-unstyled menu-elements">
       <li class="active">
-        <a class="scroll-link" href="#top-content"
+        <a  href="{{ route('home.pages.index') }}"
           ><i class="fas fa-home"></i> خانه</a
         >
       </li>
       <li>
-        <a class="scroll-link" href="#section-1"
+        <a  href="{{ route('home.pages.services') }}"
           ><i class="fas fa-briefcase"></i> خدمات بازرگانی</a
         >
       </li>
       <li>
-        <a class="scroll-link" href="{{ route('product') }}"
+        <a  href="{{ route('product') }}"
           ><i class="fas fa-tags"></i> محصولات</a
         >
       </li>
       <li>
-        <a class="scroll-link" href="#section-5"
+        <a  href="{{ route('home.pages.blog') }}"
           ><i class="fas fa-pencil-alt"></i> وبلاگ</a
         >
       </li>
       <li>
-        <a class="scroll-link" href="#section-6"
+        <a  href="{{ route('home.pages.contact-us') }}"
           ><i class="fas fa-envelope"></i> تماس با ما</a
         >
       </li>
       <li>
-        <a class="scroll-link" href="#section-6"
+        <a  href="{{ route('home.pages.about-us') }}"
           ><i class="fas fa-address-card"></i> درباره ما</a
         >
       </li>
       <li>
-        <a class="scroll-link" href="#section-6"
+        <a  href="{{ route('home.pages.collab') }}"
           ><i class="fas fa-user-friends"></i> همکاری با ما</a
         >
       </li>
