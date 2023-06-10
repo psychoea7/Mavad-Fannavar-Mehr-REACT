1b3b5b1cb32407f03fa579da0baec302723ca7b7
psychoea7 <sohrabnasehi.sn@gmail.com>
2023-06-10 05:59:43 +0330

    code fix

--- src/screens/BlogExpand.js
+++ src/screens/BlogExpand.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-
 function BlogExpand() {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(`http://app.mavad-fannavar-mehr.ir/api/getBlog/${id}`)
@@ -20,23 +20,25 @@ function BlogExpand() {
       })
       .catch((error) => {
         console.error("Error:", error);
+        setError(error.message);
       });
-      console.log(id);
+    console.log(id);
   }, [id]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   if (!blog) {
     return <div>Loading...</div>;
   }
 
   return (
-    <>
-      <div>
-        <img src={blog.image} alt={blog.title} />
-        <h1>{blog.title}</h1>
-        <p>{blog.text}</p>
-       
-      </div>
-    </>
+    <div>
+      {blog && <img src={blog.image} alt={blog.title} />}
+      {blog && <h1>{blog.title}</h1>}
+      {blog && <p>{blog.text}</p>}
+    </div>
   );
 }
 
