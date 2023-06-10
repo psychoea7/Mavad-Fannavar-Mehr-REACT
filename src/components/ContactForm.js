58e18cf3f55077c967966154a479319cacd70150
psychoea7 <sohrabnasehi.sn@gmail.com>
2023-06-10 06:06:52 +0330

    code fix on contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,43 +9,50 @@ function ContactForm() {
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
-  console.log(errors);
-  return (
-    <>
-      <div className="contactScreen__form">
-        <h2>ارتباط با ما</h2>
-        <span>نظرات خود را با ما در میان بگذارید.</span>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder="نام و نام خانوادگی"
-            {...register("name", { required: true, maxLength: 80 })}
-          />
-          <input
-            type="text"
-            placeholder="نام شرکت"
-            {...register("company name", { required: false, maxLength: 100 })}
-          />
-          <input
-            type="text"
-            placeholder="پست الکترونیک"
-            {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
-          />
-          <input
-            type="tel"
-            placeholder="تلفن همراه"
-            {...register("Mobile number", {
-              required: true,
-              minLength: 6,
-              maxLength: 12,
-            })}
-          />
-          <textarea placeholder="پیام" {...register("Message", {})} />
 
-          <input type="submit" value="ارسال" />
-        </form>
-      </div>
-    </>
+  return (
+    <div className="contactScreen__form">
+      <h2>ارتباط با ما</h2>
+      <span>نظرات خود را با ما در میان بگذارید.</span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="نام و نام خانوادگی"
+          {...register("name", { required: true, maxLength: 80 })}
+        />
+        {errors.name && <span className="text-danger">وارد کردن نام الزامی است.</span>}
+
+        <input
+          type="text"
+          placeholder="نام شرکت"
+          {...register("company_name", { maxLength: 100 })}
+        />
+
+        <input
+          type="text"
+          placeholder="پست الکترونیک"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <span className="text-danger">لطفاً یک آدرس ایمیل معتبر وارد کنید.</span>}
+
+        <input
+          type="tel"
+          placeholder="تلفن همراه"
+          {...register("mobile_number", {
+            required: true,
+            minLength: 6,
+            maxLength: 12,
+          })}
+        />
+        {errors.mobile_number && (
+          <span className="text-danger">تلفن همراه باید بین 6 تا 12 رقم باشد.</span>
+        )}
+
+        <textarea placeholder="پیام" {...register("message")} />
+
+        <input type="submit" value="ارسال" />
+      </form>
+    </div>
   );
 }
 
