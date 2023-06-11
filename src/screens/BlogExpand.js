dbcdbc86e43c775921a6a9b7a437c55d340a1c9f
psychoea7 <sohrabnasehi.sn@gmail.com>
2023-06-11 03:54:07 +0330

    fix on blog expand

--- src/screens/BlogExpand.js
+++ src/screens/BlogExpand.js
@@ -1,28 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function BlogExpand() {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
   const [error, setError] = useState(null);
+  
+  // Let's establish the base URL
+  const BASE_URL = "http://app.mavad-fannavar-mehr.ir";
+
+  const fetchBlog = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/getBlog?id=${id}`);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      console.log('Data:', data);
+
+      if (data) {
+        setBlog(data);
+      } else {
+        throw new Error("Blog data is not available");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
-    fetch(`http://app.mavad-fannavar-mehr.ir/api/getBlog/${id}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlog(data.blog);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        setError(error.message);
-      });
-    console.log(id);
+    fetchBlog();
   }, [id]);
 
   if (error) {
@@ -33,11 +44,16 @@ function BlogExpand() {
     return <div>Loading...</div>;
   }
 
+  const imageUrl = `${BASE_URL}/${blog.image}`; // Prepend base URL to the image path
+
+  // New! Let's log that image URL and see what we're dealing with.
+  console.log("Image URL:", imageUrl);
+
   return (
     <div>
-      {blog && <img src={blog.image} alt={blog.title} />}
-      {blog && <h1>{blog.title}</h1>}
-      {blog && <p>{blog.text}</p>}
+      <img src={imageUrl} alt={blog.title} />
+      <h1>{blog.title}</h1>
+      <p dangerouslySetInnerHTML={{ __html: blog.text }} />
     </div>
   );
 }
